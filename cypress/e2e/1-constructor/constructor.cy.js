1430718a18590c2ad815f4112817c3a23bc1dadd
divirus <6b6D7ogi>
2023-03-30 22:19:08 +0300

    cypress test fix

--- cypress/e2e/1-constructor/constructor.cy.js
+++ cypress/e2e/1-constructor/constructor.cy.js
@@ -1,10 +1,10 @@
 describe('drag and drop in construtor works correctly', function() {
-  before(function() {
+  beforeEach(function() {
     cy.visit('http://localhost:3000');
   });
 
   it('should load ingredients data', function() {
-    cy.get('[class^=burger-ingredients-card_ingredient_card__]')
+    cy.get('[class^=ingredient-details-card_card__]')
       .first()
       .as('bun');
 
@@ -21,12 +21,12 @@ describe('drag and drop in construtor works correctly', function() {
   })
 
   it('should drag and drop bun to the constructor', function() {
-    cy.get('#bun')
-      .find('[class^=burger-ingredients-card_ingredient_card__]')
+    cy.get('#ingredients-block-1')
+      .find('[class^=ingredient-details-card_card__]')
       .first()
       .as('bun')
 
-    cy.get('[class^=burger-constructor_burger_constructor_list__]')
+    cy.get('[class^=burger-constructor_list__]')
       .children()
       .first()
       .as('topBun')
@@ -40,22 +40,22 @@ describe('drag and drop in construtor works correctly', function() {
   });
   
   it('should drag and drop ingredients', function() {
-    cy.get('#sauce')
-      .find('[class^=burger-ingredients-card_ingredient_card__]')
+    cy.get('#ingredients-block-2')
+      .find('[class^=ingredient-details-card_card__]')
       .first()
       .as('sauce')
 
-    cy.get('#main')
-      .find('[class^=burger-ingredients-card_ingredient_card__]')
+    cy.get('#ingredients-block-3')
+      .find('[class^=ingredient-details-card_card__]')
       .first()
       .as('main')
 
-    cy.get('#main')
-      .find('[class^=burger-ingredients-card_ingredient_card__]')
+    cy.get('#ingredients-block-3')
+      .find('[class^=ingredient-details-card_card__]')
       .last()
       .as('main2')
 
-    cy.get('[class^=burger-constructor_burger_constructor_list__]')
+    cy.get('[class^=burger-constructor_list__]')
       .children()
       .next()
       .first()
@@ -81,31 +81,36 @@ describe('drag and drop in construtor works correctly', function() {
       .should('contain', 'Сыр с астероидной плесенью')      
   });
 
-  it('should rearrange ingredients in constructor', function() {
-    cy.get('[class^=draggable-constructor-element_draggable_list_item__]')
+  it('should remove first ingredient from constructor', function() {
+    cy.get('#ingredients-block-2')
+      .find('[class^=ingredient-details-card_card__]')
       .first()
-      .as('ingredient')
+      .as('sauce')
 
-    cy.get('[class^=burger-constructor_burger_constructor_draggable_list__]')
+    cy.get('#ingredients-block-3')
+      .find('[class^=ingredient-details-card_card__]')
+      .first()
+      .as('main')
+
+    cy.get('[class^=burger-constructor_list__]')
+      .children()
+      .next()
       .first()
       .as('ingredientsList')
 
-    cy.get('@ingredient').trigger('dragstart');
-    cy.get('@ingredientsList').trigger('dragenter').trigger('drop');
+    // drop 1st ingredient
+    cy.get('@sauce').trigger('dragstart');
+    cy.get('@ingredientsList').trigger('drop');
 
-    // the first main ingredient should be now on the top of constructor
-    cy.get('@ingredientsList')
-      .find('.constructor-element__text')
-      .first()
-      .should('contain', 'Филе Люминесцентного тетраодонтимформа')
-  });
+    // drop 2nd ingredient
+    cy.get('@main').trigger('dragstart');
+    cy.get('@ingredientsList').trigger('drop');
 
-  it('should remove first ingredient from constructor', function() {
-    cy.get('[class^=draggable-constructor-element_draggable_list_item__]')
+    cy.get('[class^=draggable-element_draggable_list_item__]')
       .first()
       .as('ingredient')
 
-    cy.get('[class^=burger-constructor_burger_constructor_draggable_list__]')
+    cy.get('[class^=burger-constructor_draggable_list__]')
       .first()
       .as('ingredientsList')
 
@@ -116,6 +121,6 @@ describe('drag and drop in construtor works correctly', function() {
     // check for the deleted ingredients name in the list
     cy.get('@ingredientsList')
       .find('.constructor-element__text')
-      .should('not.contain', 'Филе Люминесцентного тетраодонтимформа')
+      .should('not.contain', 'Соус Spicy-X')
   });
 }); 
